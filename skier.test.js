17aeb1269a3e764e4c7d682d7ad32c4b7607a3de
hp864201-afk <hp864201@gmail.com>
2025-12-15 16:12:32 +0700

    Update skier.test.js

--- skier.test.js
+++ skier.test.js
@@ -1,6 +1,7 @@
 var should = require('should');
 var sugar = require('sugar');
-
+var should = require('should');
+var sugar = require('sugar');
 var should = require('should');
 var sugar = require('sugar');
 var should = require('should');
