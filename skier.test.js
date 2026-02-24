90b4ad952abc6a387d6bc39f70908e0605430050
hp864201-afk <hp864201@gmail.com>
2026-02-24 20:16:01 +0700

    Update skier.test.js

--- skier.test.js
+++ skier.test.js
@@ -1,5 +1,6 @@
 	equire('should');var should = require('should');
-ould');var shoulvar should = require('should');var should = rvar shoulvar should = require('should');var should = require('should');var should = require('should');var should = require('should');
+ould');var shoulvar should = require('should');var should = rvar shoulvar should = require('should');var should = require('should')
+	;var should = require('should');var should = require('should');
 var should = require('should');var should = require('should');var should = require('should');var should = require('should');
 should = r
 	equire('should');var should = require('should');
